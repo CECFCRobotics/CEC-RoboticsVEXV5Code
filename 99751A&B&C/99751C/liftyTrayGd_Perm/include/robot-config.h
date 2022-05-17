2634b6bbb12eca85c854e6b9ec4c59feba945b5e
dbgoldfain <55030435+dbgoldfain@users.noreply.github.com>
2022-05-17 17:04:21 -0600

    Create robot-config.h

---
+++ 99751A&B&C/99751C/liftyTrayGd_Perm/include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+extern motor leftMotor;
+extern motor rightMotor;
+extern motor mMotor;
+extern motor lBar;
+extern motor rBar;
+extern motor lIntake;
+extern motor rIntake;
+extern motor dump;
+
+extern motor_group DT;
+extern motor_group DTM;
+extern motor_group bar;
+extern motor_group intake;
+extern motor_group all;
+
+extern controller Houston;
+extern controller Austin;
+
+void vexcodeInit(void);
