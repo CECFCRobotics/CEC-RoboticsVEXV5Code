a984bf789b8862b0d5ac4d95af8fd3b9db6a3d48
dbgoldfain <55030435+dbgoldfain@users.noreply.github.com>
2022-05-16 18:21:58 -0600

    Create robot-config.h

---
+++ 99751A&B&C/99751C/The Republican State/include/robot-config.h
@@ -0,0 +1,22 @@
+using namespace vex;
+//Brain
+extern brain Brain;
+//Motor
+extern motor RightDrive;
+extern motor LeftDrive;
+extern motor H_Drive;
+extern motor LeftIntake;
+extern motor RightIntake;
+extern motor Lift;
+extern motor IntakeLift;
+extern motor IntakeLift2;
+extern motor_group DT;
+extern motor_group intakeLift;
+extern motor_group intake;
+//Controllers
+extern controller Controller;
+extern controller Controller2;
+//Sensors
+extern pot PotentiometerL;
+extern pot PotentiometerR;
+void  vexcodeInit( void );
